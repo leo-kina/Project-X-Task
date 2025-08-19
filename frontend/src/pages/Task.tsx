30a72a3187c4666babfefccbcf094ead76ca1a94
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 12:26:46 -0300

    feat: Adicionando input de task

--- frontend/src/pages/Task.tsx
+++ frontend/src/pages/Task.tsx
@@ -1,4 +1,6 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { BarChart2, MoonIcon, SunIcon } from 'lucide-react'
 import React from 'react'
 
 interface HomeProps{
@@ -8,8 +10,27 @@ interface HomeProps{
 export const Task = ({homeBtn}:HomeProps) => {
   return (
     <div>
-        <h2>Tasks</h2>
-        <Button onClick={homeBtn}>Clique aki</Button>
+        <div className='flex justify-between mb-5'>
+        <div className='flex items-center gap-3'>
+                <BarChart2 color='blue' />
+        <h2 className='font-bold text-accent-foreground text-2xl'>Project <span className='text-blue-700'>X</span> Tasks</h2>
+    
+        </div>
+        <div className='flex gap-'>
+        <MoonIcon/>
+        <SunIcon/>
+        </div>
+        </div>
+        <div className='flex p-10 items-center gap-2 justify-center'>
+        <Input className='w-full max-w-xl' placeholder='Digite seu Projeto'/>
+          <Button type="submit" variant="outline">Enviar</Button>
+        </div>
+        <div>
+            
+        </div>
+        <Button onClick={homeBtn}>Ir para Home</Button>
+
+        
     </div>
   )
 }
