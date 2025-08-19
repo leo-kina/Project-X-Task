24e9a877f806651d54c0a0386d2990e8de3c11de
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 20:20:49 -0300

    feat: adicionando usuarioController

--- backend/src/main/java/com/leokina/ProjectXTask/Usuarios/UsuariosController.java
+++ backend/src/main/java/com/leokina/ProjectXTask/Usuarios/UsuariosController.java
@@ -1,4 +1,16 @@
 package com.leokina.ProjectXTask.Usuarios;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuariosController {
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "boas vindas meu parceiro";
+    }
 }
