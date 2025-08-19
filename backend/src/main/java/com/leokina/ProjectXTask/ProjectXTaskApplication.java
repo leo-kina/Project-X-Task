d6674fa938d7104d0608bfddad5ee79fe933dcb8
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 11:30:59 -0300

    first commit

---
+++ backend/src/main/java/com/leokina/ProjectXTask/ProjectXTaskApplication.java
@@ -0,0 +1,13 @@
+package com.leokina.ProjectXTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectXTaskApplication.class, args);
+	}
+
+}
