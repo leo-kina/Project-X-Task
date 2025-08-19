c43a199b72a4057174af5ba0efc84168af501c47
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 20:22:26 -0300

    feat: adicionando usuarioRepository

---
+++ backend/src/main/java/com/leokina/ProjectXTask/Usuarios/UsuariosRepository.java
@@ -0,0 +1,6 @@
+package com.leokina.ProjectXTask.Usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuariosRepository extends JpaRepository<UsuariosModel ,Long> {
+}
