b91a2b60688f29d88b4e8ba6e118a05f8ddff768
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 22:46:51 -0300

    feat: projetos DTO

--- backend/src/main/java/com/leokina/ProjectXTask/Projetos/ProjetosDTO.java
+++ backend/src/main/java/com/leokina/ProjectXTask/Projetos/ProjetosDTO.java
@@ -1,4 +1,25 @@
 package com.leokina.ProjectXTask.Projetos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.leokina.ProjectXTask.Usuarios.UsuariosDTO;
+import com.leokina.ProjectXTask.Usuarios.UsuariosModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class ProjetosDTO {
+    private Long id;
+
+
+    private String name;
+
+
+    private List<UsuariosDTO> usuario;
 }
