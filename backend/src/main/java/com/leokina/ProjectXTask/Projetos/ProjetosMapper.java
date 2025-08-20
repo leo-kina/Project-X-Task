1ea88da9015cbd1d1ee9b413bf000ebdfa883800
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 22:47:08 -0300

    feat: Projetos Mapper

--- backend/src/main/java/com/leokina/ProjectXTask/Projetos/ProjetosMapper.java
+++ backend/src/main/java/com/leokina/ProjectXTask/Projetos/ProjetosMapper.java
@@ -1,4 +1,22 @@
 package com.leokina.ProjectXTask.Projetos;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProjetosMapper {
+    public ProjetosModel map(ProjetosDTO projetosDTO){
+        ProjetosModel projetosModel = new ProjetosModel();
+        projetosModel.setName(projetosDTO.getName());
+        projetosModel.setId(projetosDTO.getId());
+        projetosModel.setUsuario(projetosDTO.getUsuario());
+
+        return projetosModel;
+    }
+    public ProjetosDTO map(ProjetosModel projetosModel){
+        ProjetosDTO projetosDTO = new ProjetosDTO();
+        projetosDTO.setId(projetosModel.getId());
+        projetosDTO.setName(projetosModel.getName());
+        projetosDTO.setUsuario(projetosModel.getUsuario());
+        return projetosDTO;
+    }
 }
