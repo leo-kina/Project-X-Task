7177b0d154cf22bae55a6871b9f68ee8081755fc
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 22:46:12 -0300

    feat: projetos controller

--- backend/src/main/java/com/leokina/ProjectXTask/Projetos/ProjetosController.java
+++ backend/src/main/java/com/leokina/ProjectXTask/Projetos/ProjetosController.java
@@ -1,4 +1,30 @@
 package com.leokina.ProjectXTask.Projetos;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projetos")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProjetosController {
+    private ProjetoService projetoService;
+
+    public ProjetosController(ProjetoService projetoService) {
+        this.projetoService = projetoService;
+    }
+
+   @GetMapping("/listar")
+    public List<ProjetosDTO> listarProjetos(){
+        return projetoService.listarProjetos();
+    }
+    @PostMapping("/criar")
+    public ProjetosDTO criarProjetos(@RequestBody ProjetosDTO projetosDTO){
+        return projetoService.criarProjeto(projetosDTO);
+    }
+    @DeleteMapping("/deletar/{id}")
+    public void deletarUser(@PathVariable Long id){
+        projetoService.deletarProjeto(id);
+
+    }
 }
