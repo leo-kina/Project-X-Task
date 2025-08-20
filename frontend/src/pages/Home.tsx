b2bd95676a2aeb24de6b47df0128e3810f753d78
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-19 23:20:32 -0300

    feat: home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,10 +7,12 @@ interface SobreProps{
 }
 export const Home = ({tasksBtn, funcionariosBtn}:SobreProps) => {
   return (
-    <div>
-        <h2>Bem vindo</h2>
+    <div className='flex items-center justify-center h-screen flex-col'>
+        <h2 className='font-medium'>Bem vindo</h2>
+        <div className='flex gap-10'>
          <Button onClick={tasksBtn}>tasks</Button>
          <Button onClick={funcionariosBtn}>funcionarios</Button>
+         </div>
     </div>
    
   )
